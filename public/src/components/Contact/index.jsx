12296ced2c1e15c84e94c030281690d3a2d79e9e
blake-swl <seulwoolee93@gmail.com>
2018-09-18 19:39:02 -0700

    [modify] - changing contact component layout

--- public/src/components/Contact/index.jsx
+++ public/src/components/Contact/index.jsx
@@ -9,17 +9,21 @@ export default class Contact extends Component {
 
     this.state = {
       message: '',
+      name: '',
+      email: '',
     }
+    this.onChange = this.onChange.bind(this);
   }
 
-  handleInput(e) {
+  onChange(e) {
     e.preventDefault();
     this.setState({
-      message: e.target.value
+      [e.target.name]: e.target.value
     })
-    console.log(this.state.message);
+    console.log(this.state);
   }
   // Note to self: conditionally render Typed when user scrolls to current component
+  // phone number, linkedin and git will be hovers with links/numbers as afters
 
   render() {
     return(
@@ -29,20 +33,32 @@ export default class Contact extends Component {
           typeSpeed={80}/>
         </div>
         <div className={styles.bottom}>
-          <div className={styles.left}>            {/* <div className={styles.leftSub}> */}
-              <div className={styles.heading}>Contact Info</div>
-              <span className={styles.phone}>Phone</span>
-              <span className={styles.number}>(323) 482-0091</span>
-              <div className={styles.mailbox}>
-                <form onSubmit={e => this.handleSend(e)}>
-                  <textarea 
-                    className={styles.text} 
-                    placeholder="Write your message here" 
-                    onChange={e => this.handleInput(e)}>
-                  </textarea>
-                  <button type="sumbit">Send</button>
-                </form>
+          <div className={styles.left}>
+            <div className={styles.heading}>Contact Info</div>
+              <div className={styles.leftSub}>
+                <span className={styles.phone}>Phone</span>
+                <span className={styles.number}>(323) 482-0091</span>
               </div>
+            <div className={styles.mailbox}>
+              <form onSubmit={e => this.handleSend(e)}>
+                <textarea 
+                  className={styles.name} 
+                  placeholder="Name"
+                  onChange={this.onChange}>
+                </textarea>
+                <textarea 
+                  className={styles.email}
+                  placeholder="Email"
+                  onChange={this.onChange}>
+                </textarea>
+                <textarea 
+                  className={styles.text} 
+                  placeholder="Write your message here" 
+                  onChange={this.onChange}>
+                </textarea>
+                <button className={styles.send} type="sumbit">Send</button>
+              </form>
+            </div>
           </div>
           <div className={styles.right}>
             address
