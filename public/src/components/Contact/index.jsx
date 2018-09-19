4e7f2854432ebfa5567fcf900c137e4ff9ba7e80
blake-swl <seulwoolee93@gmail.com>
2018-09-18 17:14:26 -0700

    [modify] - made navbar transparent

--- public/src/components/Contact/index.jsx
+++ public/src/components/Contact/index.jsx
@@ -31,11 +31,15 @@ export default class Contact extends Component {
           <div className={styles.left}>
             <div className={styles.leftCon}>
               <div className={styles.heading}>Contact Info</div>
-              {/* <span className={styles.phone}>Phone</span> */}
+              <span className={styles.phone}>Phone</span>
               <span className={styles.number}>(323) 482-0091</span>
               <div className={styles.mailbox}>
                 <form onSubmit={e => this.handleSend(e)}>
-                  <textarea placeholder="Write your message here" onChange={e => this.handleInput(e)}></textarea>
+                  <textarea 
+                    className={styles.text} 
+                    placeholder="Write your message here" 
+                    onChange={e => this.handleInput(e)}>
+                  </textarea>
                   <button type="sumbit">Send</button>
                 </form>
               </div>
