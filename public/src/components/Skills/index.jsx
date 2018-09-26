6bd3fdc43e3cc932dec08bacd60837f71fac4819
blake-swl <seulwoolee93@gmail.com>
2018-09-25 17:11:56 -0700

    [fix] - skills component color scheme

--- public/src/components/Skills/index.jsx
+++ public/src/components/Skills/index.jsx
@@ -12,22 +12,22 @@ const Skills = () => {
       </div>
       <div className="mainSkills">
         <div className="javascript">JavaScript 
-          <Progress percent={95} theme={{ active: {trailColor: '#535B63', color: '#8AF3FF'}}}/>
+          <Progress percent={95} theme={{ active: {trailColor: '#535B63', color: '#E71D36'}}}/>
         </div>
         <div className="react">React
-          <Progress percent={90} theme={{ active: {trailColor: '#535B63', color: '#8AF3FF'}}}/>
+          <Progress percent={90} theme={{ active: {trailColor: '#535B63', color: '#E71D36'}}}/>
         </div>
         <div className="node">Node
-          <Progress percent={85} theme={{ active: {trailColor: '#535B63', color: '#8AF3FF'}}}/>
+          <Progress percent={85} theme={{ active: {trailColor: '#535B63', color: '#E71D36'}}}/>
         </div>
         <div className="htmlcss">HTML5/CSS3
-          <Progress percent={92} theme={{ active: {trailColor: '#535B63', color: '#8AF3FF'}}}/>
+          <Progress percent={92} theme={{ active: {trailColor: '#535B63', color: '#E71D36'}}}/>
         </div>
         <div className="sql">SQL/NoSQL
-          <Progress percent={90} theme={{ active: {trailColor: '#535B63', color: '#8AF3FF'}}}/>
+          <Progress percent={90} theme={{ active: {trailColor: '#535B63', color: '#E71D36'}}}/>
         </div>
         <div className="graphql">GraphQL
-          <Progress percent={85} theme={{ active: {trailColor: '#535B63', color: '#8AF3FF'}}}/>
+          <Progress percent={85} theme={{ active: {trailColor: '#535B63', color: '#E71D36'}}}/>
         </div>
       </div>
       <div className="title2">
@@ -35,13 +35,13 @@ const Skills = () => {
       </div>
       <div className="moreSkills">
         <Progress className="check1"
-        type="circle" percent={100} theme={{ active: {trailColor: '#535B63', color: '#8AF3FF'}}} />
+        type="circle" percent={100} theme={{ active: {trailColor: '#535B63', color: '#E71D36'}}} />
         <div className="aws">AWS EC2</div>
         <Progress className="check2"
-        type="circle" percent={100} theme={{ active: {trailColor: '#535B63', color: '#8AF3FF'}}} />       
+        type="circle" percent={100} theme={{ active: {trailColor: '#535B63', color: '#E71D36'}}} />       
         <div className="jquery">jQuery</div>
         <Progress className="check3"
-        type="circle" percent={100} theme={{ active: {trailColor: '#535B63', color: '#8AF3FF'}}} />
+        type="circle" percent={100} theme={{ active: {trailColor: '#535B63', color: '#E71D36'}}} />
         <div className="git">Git Workflow</div>
       </div>
     </div>
@@ -49,5 +49,5 @@ const Skills = () => {
 };
 
 export default Skills;
-          // #8AF3FF
+          // #E71D36
           // #535B63
