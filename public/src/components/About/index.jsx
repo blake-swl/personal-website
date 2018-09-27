6f51e357044d0054c803c4cd7529727e493c9d22
blake-swl <seulwoolee93@gmail.com>
2018-09-26 18:33:36 -0700

    [add] - updated about me section

--- public/src/components/About/index.jsx
+++ public/src/components/About/index.jsx
@@ -39,8 +39,10 @@ const About = () => {
       <div className="gallery">
         <div className="blur">
             <div className="personal">
-              <h1>Get to know me</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur efficitur suscipit dictum. Morbi quis turpis aliquam sem dignissim ornare. Sed aliquam dui ex, sed scelerisque dolor vehicula sed. Ut consequat libero nibh, quis tempor erat aliquam non. Cras egestas odio a mi placerat, imperdiet placerat ipsum ornare. Phasellus et mattis sapien. Suspendisse imperdiet massa nulla, quis mattis magna elementum malesuada. Mauris venenatis purus lorem, et ornare erat egestas id. Pellentesque molestie, leo sed tincidunt viverra, nibh erat semper sapien, eget scelerisque enim leo non lorem. Nam nec lobortis ipsum.</p>
+              <h1>Get to know me more</h1>
+              <p>I’m a software engineer with a unique perspective and upbringing that is ready to give back and serve the community with my experiences, talents, and lessons.
+Born in Seoul, Korea, I grew up in Saint Petersburg, Russia from the ages of two to 12, until I moved to Los Angeles, CA where I currently reside. Having lived in such distinct cultures I bring a nuanced approach to social interactions and strong communication skills to seamlessly blend and adapt to any culture. Furthermore, coming from a family of musicians and artists, I possess a keen eye for art and design. In college, I earned my bachelors in psychology at UCSB and pursued a career in pharmacology until I dabbled in coding and it changed my life. I’m a notorious coffee enthusiast, lover of nature, Lebron James fanatic, and (hopefully) the best son to my loving parents.
+</p>
             </div>
           <div className="flex">
             <div className="pictures">
--- public/src/components/Works/index.jsx
+++ public/src/components/Works/index.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import './works.scss';
 import airbnb from '../../../dist/images/airbnb.jpg';
+import amazon from '../../../dist/images/amazon.jpg';
+import homepage from '../../../dist/images/homepage.jpg';
 
 export default class Works extends Component {
   render() {
     return(
       <div className="works">
-        Works.jsx
-        <img src={airbnb} alt="airbnb"/>
+        <h1>Works</h1>
+          <div className="portfolio">
+            {/* <div className="film"> */}
+                <div className="amazon">
+
+                  {/* <img src={amazon} className="film"/> */}
+                  <h1>Amazon</h1>
+                </div>
+                <div className="airbnb">
+                  {/* <img src={airbnb} className="film"/> */}
+                  <h1>Airbnb</h1>
+                </div>
+                <div className="homepage">
+                  {/* <img src={homepage} className="film"/> */}
+                  <h1>homepage</h1>
+                </div>
+            {/* </div> */}
+          </div>
       </div>
     )
   }
-}
+};
+
+// create gif-able images
