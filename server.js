62ffbd0511d9524738608d1f2b73a73a2e10f7d3
blake-swl <seulwoolee93@gmail.com>
2018-10-11 16:22:43 -0700

    [fix] - deployment issues

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const path = require('path');
 const parser = require('body-parser');
 
 const app = express();
-const PORT = 9001;
+const PORT = process.env.PORT || 9001;
 
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, './public/dist')));
 
+app.get(' * ', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public/dist/index.html'))
+});
 
 app.listen(PORT, () => console.log(`Successfully connected to PORT: ${PORT}`));
