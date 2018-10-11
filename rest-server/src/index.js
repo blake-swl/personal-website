2551a9f25ff0241fa61beb79c7446bf4ddc114d3
blake-swl <seulwoolee93@gmail.com>
2018-10-10 17:53:41 -0700

    [edit] - changed rest-server configuration

---
+++ rest-server/src/index.js
@@ -0,0 +1,18 @@
+import { createServer } from 'http';
+import { success } from './lib'
+
+import App from './config';
+
+const app = App.express;
+
+const server = createServer( app );
+const PORT = process.env.PORT || 9002;
+
+server.listen( PORT, err => {
+  if (err) {
+    throw new Error;
+  }
+  success(`Server is now listening on PORT: ${PORT}`);
+});
+
+export default server;
