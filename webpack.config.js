1a331171353f12473d623000e05179f26d30f20c
blake-swl <seulwoolee93@gmail.com>
2018-10-26 14:22:16 -0700

    [edit] - google maps api integration

--- webpack.config.js
+++ webpack.config.js
@@ -62,8 +62,11 @@ module.exports = {
   },
   mode: 'production',
   plugins: [
-   
- 
+    new webpack.DefinePlugin({
+      DEBUG: process.env.NODE_ENV !== 'production',
+          'process.env': {
+              'NODE_ENV': JSON.stringify(process.env.NODE_ENV || "development"),
+              'REACT_APP_API_KEY': JSON.stringify(process.env.REACT_APP_API_KEY)}}),
     new HtmlWebpackPlugin({
       // injects bundle.js to our new index.html
       inject: true,
