a5e2622aae464dbd3f5c12ab71dd38e76c5786de
blake-swl <seulwoolee93@gmail.com>
2018-10-28 21:23:06 -0700

    [fix] - drop down nav animation

--- public/src/components/Navigation/index.jsx
+++ public/src/components/Navigation/index.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './nav.scss';
 import 'smoothscroll';
+import 'animate.css';
+import { pulse } from 'react-animations';
 
 import { Icon } from 'react-icons-kit';
 import {navicon} from 'react-icons-kit/fa/navicon';
-
-
 
 export default class Navigation extends Component {
   constructor() {
@@ -13,31 +13,25 @@ export default class Navigation extends Component {
 
 
     // this.scrollFunction = this.scrollFunction.bind(this);
-    // this.handleScroll = this.handleScroll.bind(this);
-    // this.handleDropDown = this.handleDropDown.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
   }
-  // componentDidMount() {
-  //   window.addEventListener('scroll', this.handleScroll);
-  //   document.querySelector('.icon').addEventListener('click', this.handleDropDown);
-  // }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
   
   // Sticky Nav
-  // handleScroll = () => {  
-  //   const navbar = document.getElementById("navbar");
+  handleScroll = () => {  
+    const navbar = document.getElementById("nav");
 
-  //   const sticky = navbar.offsetTop;
+    const sticky = navbar.offsetTop;
 
-  //   if (window.pageYOffset >= sticky) {
-  //     navbar.classList.add("sticky")
-  //   } else {
-  //     navbar.classList.remove("sticky");
-  //   }
-  // }
-  // // DropDown
-  // handleDropDown = () => {
-  //   const navs = document.querySelectorAll('.navbar');
-  //   navs.forEach(nav => nav.classList.toggle('navbar_dropdown'));
-  // }
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
   // // Back to Top
   // scrollFunction = () => {
   //   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -50,49 +44,24 @@ export default class Navigation extends Component {
 
   render() {
     return (
-      <nav id="nav" className="nav">
-        <ul className="navbar">
-          <li>
-            <div className="icon" style={{ color: '#062F4f' }}>
-              <Icon icon={navicon} size={25}/>
+      <div className="navigation" id="nav">
+        <nav className="nav">
+          <div className="navbar">
+            <div>
+              <div className="icon" style={{ color: '#062F4f' }}>
+                <Icon icon={navicon} size={25}/>
+              </div>
+              <ul className="nav-con">
+                <li className="nav-item"><a className="animated pulse" href="#home">home</a></li>
+                <li className="nav-item"><a href="#about">about</a></li>
+                <li className="nav-item"><a href="#portfolio">portfolio</a></li>
+                <li className="nav-item"><a href="#skills">skills</a></li>
+                <li className="nav-item"><a href="#contact">contact</a></li>
+              </ul>
             </div>
-            <ul className="nav-item">
-              <li><a href="#home">home</a></li>
-              <li><a href="#about">about</a></li>
-              <li><a href="#portfolio">portfolio</a></li>
-              <li><a href="#skills">skills</a></li>
-              <li><a href="#contact">contact</a></li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
+          </div>
+        </nav>
+      </div>
     )
   }
 };
-
-{/* <div id="navbar">
-  <div className="navigation" >
-    <div className="icon" style={{ color: '#062F4f' }}>
-      <Icon icon={navicon} size={25}/>
-    </div>
-    <div className="nav-drop">
-      <ul className="navbar">
-        <li className="nav-item">
-          <a href="#home">home</a>
-        </li>
-        <li className="nav-item">
-          <a href="#about">about</a>
-        </li>
-        <li className="nav-item">
-          <a href="#portfolio">portfolio</a>
-        </li>
-        <li className="nav-item">
-          <a href="#skills">skills</a>
-        </li>
-        <li className="nav-item">
-          <a href="#contact">contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div> */}
