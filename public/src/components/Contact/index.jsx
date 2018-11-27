4e2ea4610d87e044805c1b4318cd82a4104d7e95
blake-swl <seulwoolee93@gmail.com>
2018-11-27 01:31:41 -0800

    [edit] - removed icons

--- public/src/components/Contact/index.jsx
+++ public/src/components/Contact/index.jsx
@@ -11,12 +11,6 @@ import { Icon } from 'react-icons-kit';
 import {location} from 'react-icons-kit/icomoon/location';
 import {phone} from 'react-icons-kit/icomoon/phone';
 import {ic_mail_outline} from 'react-icons-kit/md/ic_mail_outline';
-import {facebookSquare} from 'react-icons-kit/fa/facebookSquare';
-import {linkedinSquare} from 'react-icons-kit/fa/linkedinSquare';
-import {rebel} from 'react-icons-kit/fa/rebel';
-import {github} from 'react-icons-kit/icomoon/github';
-import {instagram} from 'react-icons-kit/fa/instagram';
-import {heart} from 'react-icons-kit/fa/heart';
 
 const googleMaps = {
   center: {
@@ -33,7 +27,7 @@ const Contact = () => {
       <div className="banner">
         <div className="banner-layer" >
           <h3 data-aos="fade-up">Let's have a chat over coffee.</h3>
-          <div className="chat" data-aos="fade-up">GET IN TOUCH.</div>
+          <div className="chat" data-aos="fade-up">GET IN TOUCH</div>
         </div>
       </div>
       <div className="bottom">
@@ -58,28 +52,11 @@ const Contact = () => {
           center={googleMaps.center}
           defaultZoom={googleMaps.zoom}
         >
-          {/* <Icon icon={heart} size={30} /> */}
         </GoogleMapReact>
       </div>
       <footer className="footer">
-        {/* <a href="https://www.facebook.com/blakeswlee">
-          <Icon icon={facebookSquare} size={50} />
-        </a> */}
-        <a href="https://github.com/blake-swl" target="_blank">
-          <Icon icon={github} size={50} />
-        </a>
-        <a href={Resume} className="tooltip" target="_blank">
-          <span className="tooltiptext">View resume</span>
-          <Icon icon={rebel} size={50} />
-        </a>
-        <a href="https://www.linkedin.com/in/blake-lee-4bb35b121/" target="_blank">
-          <Icon icon={linkedinSquare} size={50} />
-        </a>
-        {/* <a href="https://www.instagram.com/blake.swl/">
-          <Icon icon={instagram} size={50} />
-        </a> */}
+        <p className="closing-tag">Designed and developed by Blake Lee</p>
       </footer>
-      <p className="closing-tag">Designed and developed by Blake Lee</p>
 
     </div>
   )
