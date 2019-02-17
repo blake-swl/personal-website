2bf0b435fd18091e14d5e1210d92a037ba377cfa
blake-swl <seulwoolee93@gmail.com>
2019-02-17 00:12:59 -0800

    [edit] reworked color scheme: lime

--- public/src/components/Skills/index.jsx
+++ public/src/components/Skills/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './skills.scss';
 import { Progress } from 'react-sweet-progress';
 import "react-sweet-progress/lib/style.css";
-import check from '../../../dist/images/redcheck.jpg';
+import check from '../../../dist/images/greencheck.jpg';
 import anime from 'animejs';
 
 
@@ -15,25 +15,25 @@ const Skills = () => (
     </div>
     <div className="mainSkills" data-aos="fade-up">
       <div className="javascript" data-aos="fade-up">JavaScript
-        <Progress percent={90} theme={{ active: {symbol: 'Advanced', trailColor: '#535B63', color: '#D05353'}}}/>
+        <Progress percent={90} theme={{ active: {symbol: 'Advanced', trailColor: '#535B63', color: '#B5D24B'}}}/>
       </div>
       <div className="react" data-aos="fade-up">React
-        <Progress percent={95} theme={{ active: {symbol: 'Expert', trailColor: '#535B63', color: '#D05353'}}}/>
+        <Progress percent={95} theme={{ active: {symbol: 'Expert', trailColor: '#535B63', color: '#B5D24B'}}}/>
       </div>
       <div className="node" data-aos="fade-up">Node
-        <Progress percent={90} theme={{ active: {symbol: 'Advanced', trailColor: '#535B63', color: '#D05353'}}}/>
+        <Progress percent={90} theme={{ active: {symbol: 'Advanced', trailColor: '#535B63', color: '#B5D24B'}}}/>
       </div>
       <div className="htmlcss" data-aos="fade-up">HTML5/CSS3
-        <Progress percent={95} theme={{ active: {symbol: 'Expert', trailColor: '#535B63', color: '#D05353'}}}/>
+        <Progress percent={95} theme={{ active: {symbol: 'Expert', trailColor: '#535B63', color: '#B5D24B'}}}/>
       </div>
       <div className="sql" data-aos="fade-up">SQL/NoSQL
-        <Progress percent={90} theme={{ active: {symbol: 'Advanced', trailColor: '#535B63', color: '#D05353'}}}/>
+        <Progress percent={90} theme={{ active: {symbol: 'Advanced', trailColor: '#535B63', color: '#B5D24B'}}}/>
       </div>
       <div className="graphql" data-aos="fade-up">GraphQL
-        <Progress percent={85} theme={{ active: {symbol: 'Intermediate', trailColor: '#535B63', color: '#D05353'}}}/>
+        <Progress percent={85} theme={{ active: {symbol: 'Intermediate', trailColor: '#535B63', color: '#B5D24B'}}}/>
       </div>
       <div className="enthusiasm" data-aos="fade-up">Enthusiasm
-        <Progress percent={100} status="active" theme={{ active: {symbol: '👍🏻', trailColor: '#535B63', color: '#D05353'}}} />
+        <Progress percent={100} status="active" theme={{ active: {symbol: '👍🏻', trailColor: '#535B63', color: '#B5D24B'}}} />
       </div>
     </div>
     <div className="title2" data-aos="fade-up">
@@ -42,17 +42,17 @@ const Skills = () => (
     <div className="moreSkills">
       <div className="aws" data-aos="fade-up">
         <Progress className="check1"
-        type="circle" percent={100} status="active" theme={{ active: {symbol: <img src={check} style={{width: "50%"}}/> , trailColor: '#535B63', color: '#D05353'}}} />
+        type="circle" percent={100} status="active" theme={{ active: {symbol: <img src={check} style={{width: "50%"}}/> , trailColor: '#535B63', color: '#B5D24B'}}} />
         AWS EC2
       </div>
       <div className="jquery" data-aos="fade-up">
         <Progress className="check2"
-        type="circle" percent={100} status="active" theme={{ active: {symbol: <img src={check} style={{width: "50%"}}/>, trailColor: '#535B63', color: '#D05353'}}} />       
+        type="circle" percent={100} status="active" theme={{ active: {symbol: <img src={check} style={{width: "50%"}}/>, trailColor: '#535B63', color: '#B5D24B'}}} />       
         Webpack
       </div>
       <div className="git" data-aos="fade-up">
         <Progress className="check3"
-        type="circle" percent={100} status="active" theme={{ active: {symbol: <img src={check} style={{width: "50%"}}/>, trailColor: '#535B63', color: '#D05353'}}} />
+        type="circle" percent={100} status="active" theme={{ active: {symbol: <img src={check} style={{width: "50%"}}/>, trailColor: '#535B63', color: '#B5D24B'}}} />
         Git Workflow
       </div>
     </div>
